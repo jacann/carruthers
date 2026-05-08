4dfb521b65b901e9804cae0ee3885d1fe4edd15a
Jacob Annes <jnannes2@illinois.edu>
2026-05-08 04:50:15 -0500

    Fix bias calc for NFI channel images

--- biases.py
+++ biases.py
@@ -1,6 +1,8 @@
 import xarray as xr
 import numpy as np
 
+import glide.common_components.constants as constants
+
 
 def plot_biases():
         # Plot column biases for each point in time and plot
@@ -38,7 +40,7 @@ def plot_biases():
         filtered_times = times[mask]
 
         # Calculate median for this file
-        mcp_rad_top = np.median(norm_images[:, :512, :], axis=1)
+        mcp_rad_top = np.median(norm_images[:, :npix, :], axis=1)
         
         all_times.append(filtered_times)
         all_medians.append(mcp_rad_top)
@@ -62,77 +64,89 @@ def plot_biases():
 
     return
 
-def main():
-    # Plot column biases for each point in time and plot
-    filenames = ["CARRUTHERS_GCI-WFI_L1A-DRK_20251004_v1.0.nc",
-                "CARRUTHERS_GCI-WFI_L1A-DRK_20251005_v1.0.nc",
-                "CARRUTHERS_GCI-WFI_L1A-DRK_20251006_v1.0.nc",
-                "CARRUTHERS_GCI-WFI_L1A-DRK_20251007_v1.0.nc",
-                "CARRUTHERS_GCI-WFI_L1A-DRK_20251008_v1.0.nc",
-                "CARRUTHERS_GCI-WFI_L1A-DRK_20251009_v1.0.nc",
-                "CARRUTHERS_GCI-WFI_L1A-DRK_20251010_v1.0.nc",
-                "CARRUTHERS_GCI-WFI_L1A-DRK_20251011_v1.0.nc",
-                "CARRUTHERS_GCI-WFI_L1A-DRK_20251012_v1.0.nc",
-                "CARRUTHERS_GCI-WFI_L1A-DRK_20251013_v1.0.nc",
-                "CARRUTHERS_GCI-WFI_L1A-DRK_20251014_v1.0.nc",
-                "CARRUTHERS_GCI-WFI_L1A-DRK_20251015_v1.0.nc",]
+def main(n_min = 10000):
 
-
-    all_times = []
-    all_medians_top = []
-    all_medians_bottom = []
-
-    for filename in filenames:
-        ds = xr.open_dataset(f'/home/jacob/products/L1A/{filename}')
+    for imager in ["WFI", "NFI"]:
+        npix = constants.NPIX[imager]
+        half_npix = npix // 2
         
-        images = ds['images'].values
-        n_frames = ds['n_frames'].values
-        times = ds['time'].values
-        ds.close()
+        # Plot column biases for each point in time and plot
+        if imager == "WFI":
+                filenames = ["CARRUTHERS_GCI-WFI_L1A-DRK_20251004_v1.0.nc",
+                            "CARRUTHERS_GCI-WFI_L1A-DRK_20251005_v1.0.nc",
+                            "CARRUTHERS_GCI-WFI_L1A-DRK_20251006_v1.0.nc",
+                            "CARRUTHERS_GCI-WFI_L1A-DRK_20251007_v1.0.nc",
+                            "CARRUTHERS_GCI-WFI_L1A-DRK_20251008_v1.0.nc",
+                            "CARRUTHERS_GCI-WFI_L1A-DRK_20251009_v1.0.nc",
+                            "CARRUTHERS_GCI-WFI_L1A-DRK_20251010_v1.0.nc",
+                            "CARRUTHERS_GCI-WFI_L1A-DRK_20251011_v1.0.nc",
+                            "CARRUTHERS_GCI-WFI_L1A-DRK_20251012_v1.0.nc",
+                            "CARRUTHERS_GCI-WFI_L1A-DRK_20251013_v1.0.nc",
+                            "CARRUTHERS_GCI-WFI_L1A-DRK_20251014_v1.0.nc",
+                            "CARRUTHERS_GCI-WFI_L1A-DRK_20251015_v1.0.nc",]
+        elif imager == "NFI":
+                filenames = ["CARRUTHERS_GCI-NFI_L1A-DRK_20251013_v1.0.nc",
+                            "CARRUTHERS_GCI-NFI_L1A-DRK_20251014_v1.0.nc",
+                            "CARRUTHERS_GCI-NFI_L1A-DRK_20251015_v1.0.nc",]
+        else:
+            print("Invalid imager. Use 'WFI' or 'NFI'.")
+            return
 
-        # Apply mask and normalize
-        n_min = 0000
-        mask = n_frames > n_min
-        if not np.any(mask): # Skip file if no frames match criteria
-            continue
+        all_times = []
+        all_medians_top = []
+        all_medians_bottom = []
+
+        for filename in filenames:
+            ds = xr.open_dataset(f'/home/jacob/products/L1A/{filename}')
             
-        norm_images = images[mask] / n_frames[mask, np.newaxis, np.newaxis]
-        filtered_times = times[mask]
+            images = ds['images'].values
+            n_frames = ds['n_frames'].values
+            times = ds['time'].values
+            ds.close()
 
-        # Calculate median for this file
-        mcp_rad_top = np.median(norm_images[:, :256, :], axis=1)
-        mcp_rad_bottom = np.median(norm_images[:, 256:, :], axis=1)
-        
-        all_times.append(filtered_times)
-        all_medians_top.append(mcp_rad_top)
-        all_medians_bottom.append(mcp_rad_bottom)
+            # Apply mask and normalize
+            mask = n_frames > n_min
+            if not np.any(mask): # Skip file if no frames match criteria
+                continue
+                
+            norm_images = images[mask] / n_frames[mask, np.newaxis, np.newaxis]
+            filtered_times = times[mask]
 
-
-    # Concatenate all data into single arrays
-    final_times = np.concatenate(all_times)
-    final_medians_top = np.concatenate(all_medians_top, axis=0)
-    final_medians_bottom = np.concatenate(all_medians_bottom, axis=0)
-
-    # Sort by time to ensure the line doesn't "zig-zag" if files are out of order
-    sort_idx = np.argsort(final_times)
-    final_times = final_times[sort_idx]
-    final_medians_top = final_medians_top[sort_idx]
-    final_medians_bottom = final_medians_bottom[sort_idx]
+            # Calculate median for this file
+            mcp_rad_top = np.median(norm_images[:, :half_npix, :], axis=1)
+            mcp_rad_bottom = np.median(norm_images[:, half_npix:, :], axis=1)
+            
+            all_times.append(filtered_times)
+            all_medians_top.append(mcp_rad_top)
+            all_medians_bottom.append(mcp_rad_bottom)
 
 
-    print("TOP medians shape:", final_medians_top.shape)
-    print("BOTTOM medians shape:", final_medians_bottom.shape)
+        # Concatenate all data into single arrays
+        final_times = np.concatenate(all_times)
+        final_medians_top = np.concatenate(all_medians_top, axis=0)
+        final_medians_bottom = np.concatenate(all_medians_bottom, axis=0)
 
-    # Find 512 medians for each column across all images and save as numpy arrays
-    top_med_medians = np.zeros((final_medians_top.shape[1]))
-    bottom_med_medians = np.zeros((final_medians_bottom.shape[1]))
-    #print(final_medians_top.shape)
+        # Sort by time to ensure the line doesn't "zig-zag" if files are out of order
+        sort_idx = np.argsort(final_times)
+        final_times = final_times[sort_idx]
+        final_medians_top = final_medians_top[sort_idx]
+        final_medians_bottom = final_medians_bottom[sort_idx]
 
-    top_med_medians = np.median(final_medians_top, axis=0)
-    bottom_med_medians = np.median(final_medians_bottom, axis=0)
 
-    np.save('products/column_bias_top_v2.npy', top_med_medians)
-    np.save('products/column_bias_bottom_v2.npy', bottom_med_medians)
+        print("TOP medians shape:", final_medians_top.shape)
+        print("BOTTOM medians shape:", final_medians_bottom.shape)
+
+        # Find medians for each column across all images and save as numpy arrays
+        top_med_medians = np.zeros((final_medians_top.shape[1]))
+        bottom_med_medians = np.zeros((final_medians_bottom.shape[1]))
+        #print(final_medians_top.shape)
+
+        top_med_medians = np.median(final_medians_top, axis=0)
+        bottom_med_medians = np.median(final_medians_bottom, axis=0)
+
+        np.save('products/COL_BIAS_' + imager + '_TOP.npy', top_med_medians)
+        np.save('products/COL_BIAS_' + imager + '_BOTTOM.npy', bottom_med_medians)
+        print(f"Saved column biases for {imager} to products/COL_BIAS_{imager}_TOP.npy and products/COL_BIAS_{imager}_BOTTOM.npy")    
 
 if __name__ == '__main__':
     main()
